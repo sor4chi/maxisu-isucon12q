70e6d832d399a1eff09d197130e09655ba2c9013
sor4chi <sorakawa0218@gmail.com>
2023-09-24 15:39:16 +0900

    Revert "perf: distinct queryにする"
    This reverts commit 90c790b14bb179593cae61139a234b65691dab70.

--- go/isuports.go
+++ go/isuports.go
@@ -569,8 +569,7 @@ func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID i
 	if err := adminDB.SelectContext(
 		ctx,
 		&vhs,
-		// "SELECT player_id, MIN(created_at) AS min_created_at FROM visit_history WHERE tenant_id = ? AND competition_id = ? GROUP BY player_id",
-		"SELECT DISTINCT player_id, MIN(created_at) AS min_created_at FROM visit_history WHERE tenant_id = ? AND competition_id = ?",
+		"SELECT player_id, MIN(created_at) AS min_created_at FROM visit_history WHERE tenant_id = ? AND competition_id = ? GROUP BY player_id",
 		tenantID,
 		comp.ID,
 	); err != nil && err != sql.ErrNoRows {
