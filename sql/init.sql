99694ddbfaa8c283591eaaa8391c4fc880f46dfa
sor4chi <sorakawa0218@gmail.com>
2023-09-24 13:50:25 +0900

    Revert "fix: index初期化クエリの改善"
    This reverts commit ec9996e041643aca4de60688174b1b66832f68ea.

--- sql/init.sql
+++ sql/init.sql
@@ -4,4 +4,6 @@ UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
 -- FROM OUR CODE ---
-ALTER TABLE `visit_history` ADD INDEX `tenant_id_competition_id_idx` (`tenant_id`, `competition_id`, `player_id`, `created_at`);
+DROP INDEX `tenant_id_competition_id_idx` ON `visit_history`;
+CREATE INDEX `tenant_id_competition_id_idx` ON `visit_history` (`tenant_id`, `competition_id`, `player_id`, `created_at`);
+
