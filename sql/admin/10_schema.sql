a9376e07a8439f425681dcb360ba2f4dfc6c778c
sor4chi <sorakawa0218@gmail.com>
2023-09-24 11:31:58 +0900

    fix: インデックス作成箇所を変更

--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -29,5 +29,3 @@ CREATE TABLE `visit_history` (
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
-
-ALTER TABLE `visit_history` ADD INDEX `tenant_id_competition_id_idx` (`tenant_id`, `competition_id`);
